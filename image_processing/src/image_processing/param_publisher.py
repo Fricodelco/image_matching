a5cd3e1975c1eca64b77af8ab126fdfdc5b45d73
fricodelco <rodion_anisimov@mail.ru>
2022-04-09 17:24:18 +0300

    add config from flash

--- image_processing/src/image_processing/param_publisher.py
+++ image_processing/src/image_processing/param_publisher.py
@@ -4,12 +4,24 @@ import os
 import yaml
 
 def load_params():
-    home = os.getenv("HOME")
-    data_path = home+'/copa5/config/config.yaml'
+    data_path = find_path_to_config()
+    if data_path is None:
+        home = os.getenv("HOME")
+        data_path = home+'/copa5/config/config.yaml'
+        print("NO EXTEND PARAM FILE, USE DEFAULT")
+    else:
+        print("USE EXTEND PARAM FILE", data_path)
     with open(data_path) as file:
         params = yaml.full_load(file)
     return params
 
+def find_path_to_config():
+    name = 'config.yaml'
+    path = '/media/'
+    for root, dirs, files in os.walk(path+os.getlogin()):
+        if name in files:
+            return os.path.join(root, name)
+    return None
 
 if __name__ == '__main__':
     rospy.init_node('book_action_client_py')
